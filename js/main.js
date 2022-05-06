8d6568ec4075407bbf2f7b89f3358ba9336272b1
Edwingzmn <eguzmann1601@alumno.ipn.mx>
2022-05-05 23:07:43 -0500

    files update

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function divemer(){
+    var contenedor = document.getElementById("divboton");
+    contenedor.style.display = "block";
+    return true;
+}
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
